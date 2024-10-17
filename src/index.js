c21f9df7f26d3f9905215c972b417d7ba3e37efb
Misa Liu <misaliu@misaliu.top>
2024-10-17 22:16:38 +0800

    feat: Generatre fake exam result

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import FridaEvent from './frida/index.js';
 
+const randomNum = (min, max) => Math.random() * (max - min + 1) + min;
+
+const getRandomPathPoint = (count = 10) => {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    result.push({
+      x: randomNum(0, 600),
+      y: randomNum(0, 600)
+    });
+  }
+
+  return [ result ];
+};
+
+const FakeExerciseResult = (exam) => {
+  const result = { ...exam };
+
+  result.correctCnt = result.questionCnt;
+  result.costTime = 1;
+
+  for (let i = 0; i < result.questions.length; i++) {
+    const question = result.questions[i];
+    const pointPaths = getRandomPathPoint();
+
+    question.status = 1;
+    question.userAnswer = question.answer;
+    question.script = JSON.stringify(pointPaths);
+    question.curTrueAnswer = {
+      recognizeResult: question.answer,
+      pathPoints: pointPaths,
+      answer: 1,
+      showReductionFraction: 0,
+    };
+  }
+
+  result.updatedTime = Date.now();
+
+  return result;
+};
+
 FridaEvent.on('exercise_start', (info) => {
   const exam = info.examVO;
-  console.log(questions);
+  const fakeResult = FakeExerciseResult(exam);
 });
